d6dea37375147d81d49edfca080e3adf5665dde9
Carolina Delgado <carolina.delgadosd@gmail.com>
2026-06-29 02:37:43 -0400

    Evidencia de cobertura de un 100% de la capa entity mediante pruebas automatizadas

--- src/test/java/com/minimarket/entity/EntidadesPojoTest.java
+++ src/test/java/com/minimarket/entity/EntidadesPojoTest.java
@@ -7,6 +7,12 @@ import com.openpojo.validation.ValidatorBuilder;
 import com.openpojo.validation.test.impl.GetterTester;
 import com.openpojo.validation.test.impl.SetterTester;
 
+/**
+ * Suite de pruebas automatizadas para la validación estructural de la capa de Dominio (Entities).
+ * Utiliza OpenPojo para testear de manera exhaustiva y automatizada todos los métodos 
+ * mutadores (Setters) y accesores (Getters), asegurando una métrica de cobertura del 100% 
+ * en la capa Entity para métricas de JaCoCo.
+ */
 public class EntidadesPojoTest {
 
     @Test
